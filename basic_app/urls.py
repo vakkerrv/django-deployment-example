1f307725d4700e50fc5adfff3f33ca0f4c5e6142
rudolf.vakker <rudolf.vakker@oliverwyman.com>
2021-03-05 23:38:56 +0000

    reload the project

--- basic_app/urls.py
+++ basic_app/urls.py
@@ -4,8 +4,7 @@ from basic_app import views
 # SET THE NAMESPACE!
 app_name = 'basic_app'
 
-# Be careful setting the name to just /login use userlogin instead!
 urlpatterns=[
-    url(r'^register/$',views.register,name='register'),
-    url(r'^user_login/$',views.user_login,name='user_login'),
+    url(r'^relative/$',views.relative,name='relative'),
+    url(r'^other/$',views.other,name='other'),
 ]
--- learning_templates/urls.py
+++ learning_templates/urls.py
@@ -1,4 +1,4 @@
-"""learning_users URL Configuration
+"""learning_templates URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -19,8 +19,6 @@ from basic_app import views
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
-    url(r'^special/',views.special,name='special'),
     url(r'^admin/', admin.site.urls),
-    url(r'^basic_app/',include('basic_app.urls')),
-    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^basic_app/',include('basic_app.urls'))
 ]
